7ae31a649352be344cc441c443391b9394461401
SPrio <mahantasupriyo@gmail.com>
2020-03-19 01:49:24 +0530

    minor changes added

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter += 2;
     });
   }
 
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'You have pressed the button this many times:',
             ),
             Text(
               '$_counter',
